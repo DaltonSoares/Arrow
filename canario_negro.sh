47961933b9e584c222b75fccb21d997f0991c4f2
Dalton Augusto Soares <daltonsoares0@gmail.com>
2018-09-02 14:26:15 +0000

    Remoção em Dev

--- canario_negro.sh
+++ canario_negro.sh
@@ -3,4 +3,3 @@ echo "Script da Canario"
 cowsay -f tux $(fortune)
 
 ping -c2 192.168.99.20 >- && echo "Com Conexão" || echo "Sem Conexão" 
-ping -c2 192.168.99.99 >- && echo "Com Conexão" || echo "Sem Conexão" 
