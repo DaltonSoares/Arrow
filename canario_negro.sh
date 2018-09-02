e2c71e40016a2803d678adda1e96c75938fc4e87
Dalton Augusto Soares <daltonsoares0@gmail.com>
2018-09-02 14:33:48 +0000

    Nova alteração em Dev

--- canario_negro.sh
+++ canario_negro.sh
@@ -2,4 +2,12 @@
 echo "Script da Canario"
 cowsay -f tux $(fortune)
 
-ping -c2 192.168.99.20 >- && echo "Com Conexão" || echo "Sem Conexão" 
+ping -c2 192.168.99.20 >- && echo "Com Conexão" || echo "Sem Conexão"
+
+echo -n "
+######################################
+#                                   #
+#    texto aleatroio                #
+#####################################
+"
+ 
